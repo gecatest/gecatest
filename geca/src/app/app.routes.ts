39d71bed7e28016cc2c8f0b478b0375123a5eb71
avktechhub <157337132+gecatest@users.noreply.github.com>
2024-02-04 19:04:06 +0530

    first page

---
+++ geca/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+        
+      },
+
+      {
+        path: '',
+        redirectTo: '',
+        pathMatch: 'full'
+      }
+];
